3ad7f6e4892acef6c8ddd02c285fb4d343d3e292
JaganSivanantham <117537577+JaganSivanantham@users.noreply.github.com>
2026-04-08 22:24:31 +0530

    Update HomeController.java

--- src/main/java/com/handyseam/app/HomeController.java
+++ src/main/java/com/handyseam/app/HomeController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Only handle the root URL "/"
+   
     @GetMapping("/")
     public String home() {
-        // Redirect users to the new Dashboard controller automatically
+       
         return "redirect:/dashboard";
     }
 
-    // REMOVED: The @GetMapping("/dashboard") method
-    // because DashboardController.java handles it now.
+   
 }
